4f3e6e023d41ba04173be6e317d04f59f76a6a54
Irfan Heru Pratomo <60539965+irfanheru66@users.noreply.github.com>
2021-01-09 00:57:11 +0700

    benerin buat presentasi

--- AR_TugasAkhir/Assets/Script/UserInterfaceButtons.cs
+++ AR_TugasAkhir/Assets/Script/UserInterfaceButtons.cs
@@ -4,18 +4,13 @@ using System.IO;
 
 public class UserInterfaceButtons : MonoBehaviour
 {
-	public float scalingSpeed = 0.03f;
+	public float scalingSpeed = 0.1f;
 	public float rotationSpeed = 70.0f;
-	public float translationSpeed = 5.0f;
 //	public GameObject Model;
 	bool repeatScaleUp = false;
 	bool repeatScaleDown = false;
 	bool repeatRotateLeft = false;
 	bool repeatRotateRight = false;
-	bool repeatPositionUp = false;
-	bool repeatPositionDown = false;
-	bool repeatPositionLeft = false;
-	bool repeatPositionRight = false;
 	
 	void Update ()
 	{
@@ -36,22 +31,6 @@ public class UserInterfaceButtons : MonoBehaviour
 		if (repeatRotateLeft) {
 			Debug.Log(repeatRotateLeft);
 			RotationLeftButton();
-		}
-
-		if (repeatPositionUp) {
-			PositionUpButton();
-		}
-
-		if (repeatPositionDown) {
-			PositionDownButton();
-		}
-
-		if (repeatPositionLeft) {
-			PositionLeftButton();
-		}
-
-		if (repeatPositionRight) {
-			PositionRightButton();
 		}
 
 	}
@@ -112,22 +91,6 @@ public class UserInterfaceButtons : MonoBehaviour
 		repeatScaleDown = true;
 		Debug.Log ("Down");
 	}
-	public void PositionDownButtonRepeat ()
-	{
-		repeatPositionDown = true;
-	}
-	public void PositionUpButtonRepeat ()
-	{
-		repeatPositionUp = true;
-	}
-	public void PositionLeftButtonRepeat ()
-	{
-		repeatPositionLeft = true;
-	}
-	public void PositionRightButtonRepeat ()
-	{
-		repeatPositionRight = true;
-	}
 	
 	public void ScaleUpButtonOff ()
 	{
@@ -151,26 +114,6 @@ public class UserInterfaceButtons : MonoBehaviour
 		repeatRotateRight = false;
 		Debug.Log ("Off");
 	}
-	public void PositionRightButtonOff ()
-	{
-		repeatPositionRight = false;
-		Debug.Log ("Off");
-	}
-	public void PositionLeftButtonOff ()
-	{
-		repeatPositionLeft = false;
-		Debug.Log ("Off");
-	}
-	public void PositionUpButtonOff ()
-	{
-		repeatPositionUp = false;
-		Debug.Log ("Off");
-	}
-	public void PositionDownButtonOff ()
-	{
-		repeatPositionDown = false;
-		Debug.Log ("Off");
-	}
 	
 	public void ScaleDownButton ()
 	{
@@ -182,34 +125,5 @@ public class UserInterfaceButtons : MonoBehaviour
          }
 	}
 
-	public void PositionUpButton ()
-	{
-		GameObject.FindWithTag ("Model").transform.Translate (0, 0, -translationSpeed * Time.deltaTime);
-	}
 
-	public void PositionDownButton ()
-	{
-
-		GameObject.FindWithTag ("Model").transform.Translate (0, 0, translationSpeed * Time.deltaTime);
-	}
-
-	public void PositionRightButton ()
-	{
-		GameObject.FindWithTag ("Model").transform.Translate (-translationSpeed * Time.deltaTime, 0, 0);
-	}
-
-	public void PositionLeftButton ()
-	{
-		GameObject.FindWithTag ("Model").transform.Translate (translationSpeed * Time.deltaTime, 0, 0);  // backward
-	}
-
-	public void ChangeScene (string a)
-	{
-		Application.LoadLevel (a);
-	}
-
-	public void AnyButton ()
-	{
-		Debug.Log ("Any");
-	}
 }
